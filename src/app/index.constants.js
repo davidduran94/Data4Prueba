00ccab3544ee383e2233fff0780dba7d3431110f
David Duran <davidduran94@gmail.com>
2016-06-18 21:15:42 -0500

    bower components

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('d3js')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
